b11ebbc46e4a91413e123cf555b1d33ffbc3a922
Niewolinsky <niewolinski@protonmail.com>
2023-01-13 14:07:33 +0100

    code refactor

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ WIP
 
 TODO:
 <!-- add metadata do json (date the data was taken, id of data, website scrapped) -->
-create database connection to persist data (either postgresql with jsonb or mongodb)
+<!-- create database connection to persist data (either postgresql with jsonb or mongodb) -->
+create api part
+consider either gocron or linux cron file to run scrapper periodically
+write simple front (svelte?) to serve data
--- main.go
+++ main.go
@@ -2,86 +2,105 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/bennyscetbun/jsongo"
 	"github.com/gocolly/colly/v2"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type Iem struct {
-	Name                  string `json:"name" bson:"name"`
-	Price                 string `json:"price" bson:"price"`
-	Price_before_discount string `json:"price_before_discount" bson:"price_before_discount"`
-	Is_unreleased         bool   `json:"is_unreleased" bson:"is_unreleased"`
+	Name                  string `bson:"name"`
+	Price                 string `bson:"price"`
+	Price_before_discount string `bson:"price_before_discount"`
+	Is_unreleased         bool   `bson:"is_unreleased"`
 }
 
 type Metadata struct {
-	Date    string `json:"date"`
-	Website string `json:"website"`
-	Id      int    `json:"id"`
+	Date      time.Time `bson:"date"`
+	Start_url string    `bson:"start_url"`
+	Scrap_id  int       `bson:"id"`
+}
+
+type ScrapResult struct {
+	Iems     []Iem
+	Metadata Metadata
+}
+
+type config struct {
+	db struct {
+		uri string
+	}
+	website struct {
+		url string
+	}
+}
+
+func initScrapper() *colly.Collector {
+	clt := colly.NewCollector()
+
+	clt.OnError(func(_ *colly.Response, err error) {
+		fmt.Fprintf(os.Stderr, "error while scraping the website: %v \n", err)
+		os.Exit(1)
+	})
+
+	return clt
+}
+
+func dbConnect(cfg config) (*mongo.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	db_connection, err := mongo.Connect(ctx, options.Client().ApplyURI(cfg.db.uri))
+	if err != nil {
+		return nil, err
+	}
+
+	err = db_connection.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	return db_connection, nil
 }
 
 func main() {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+	cfg := config{}
+	{
+		cfg.website.url = "https://hifigo.com/collections/in-ear?sort_by=price-ascending"
+		flag.StringVar(&cfg.db.uri, "db_uri", "", "MongoDB URI")
+		flag.Parse()
 	}
 
-	err = client.Ping(context.TODO(), readpref.Primary())
+	db_connection, err := dbConnect(cfg)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to ping database: %v\n", err)
+		fmt.Fprintf(os.Stderr, "connecting to database failed: %v \n", err)
 		os.Exit(1)
 	}
-
-	iems_collection := client.Database("go-iemscrapper").Collection("iems")
-
-	clt := colly.NewCollector()
-	URL := "https://hifigo.com/collections/in-ear?page=16&sort_by=price-ascending"
-
-	time_now := time.Now()
-	time_formatted := time_now.Format("2006-01-02 15:04:05")
-
-	metadata := Metadata{time_formatted, URL, 1}
+	iems_collection := db_connection.Database("go-iemscrapper").Collection("iems")
 
 	iem_list := []Iem{}
-
-	clt.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	clt.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
-
-	clt.OnResponse(func(r *colly.Response) {
-		fmt.Println("Visited", r.Request.URL)
-	})
-
+	clt := initScrapper()
+	//scrap data from current page
 	clt.OnHTML(".productitem--info", func(e *colly.HTMLElement) {
 		title := e.ChildText(".productitem--title")
 		is_unreleased := false
 
 		price_original_arr := e.ChildTexts(".price--main .money")
 		if len(price_original_arr) == 0 {
-			price_original_arr = append(price_original_arr, "0.00$")
+			price_original_arr = append(price_original_arr, "$0.00")
 			is_unreleased = true
 		}
 		price_original := price_original_arr[0]
 
 		price_discounted_arr := e.ChildTexts(".price--compare-at .money")
 		if len(price_discounted_arr) == 0 {
-			price_discounted_arr = append(price_discounted_arr, "0.00$")
+			price_discounted_arr = append(price_discounted_arr, "$0.00")
 			is_unreleased = true
 		}
 		price_discounted := price_discounted_arr[0]
@@ -92,34 +111,20 @@ func main() {
 
 		iem_list = append(iem_list, Iem{title, price_original, price_discounted, is_unreleased})
 	})
-
+	//visit next page
 	clt.OnHTML(".pagination--next", func(e *colly.HTMLElement) {
 		urlSplit := strings.SplitAfter(e.Request.URL.String(), "/")
 		baseUrl := urlSplit[0] + urlSplit[1] + urlSplit[2]
 		nextUrl := baseUrl + e.ChildAttr("a", "href")
 		e.Request.Visit(nextUrl)
 	})
+	clt.Visit(cfg.website.url)
 
-	clt.Visit(URL)
+	result := ScrapResult{iem_list, Metadata{time.Now(), cfg.website.url, 1}}
 
-	root := jsongo.Node{}
-	root.Map("iems").Val(iem_list)
-	root.Map("metadata").Val(metadata)
-	iemscrapper_data, _ := json.MarshalIndent(&root, "", "  ")
-	var doc interface{}
-	err = bson.UnmarshalExtJSON(iemscrapper_data, true, &doc)
+	response, err := iems_collection.InsertOne(context.TODO(), result)
 	if err != nil {
 		panic(err)
 	}
-
-	result, err := iems_collection.InsertOne(context.TODO(), doc)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(result.InsertedID)
-
-	err = os.WriteFile("iemscrapper_data.json", iemscrapper_data, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(response.InsertedID)
 }
