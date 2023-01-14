8a123f2a66df052152a055b14e1672cfb66fbedb
Niewolinsky <niewolinski@protonmail.com>
2023-01-14 11:57:03 +0100

    scrapping ranking website after iem website

--- main.go
+++ main.go
@@ -22,6 +22,12 @@ type Iem struct {
 	Is_unreleased         bool   `bson:"is_unreleased"`
 }
 
+type Ranking struct {
+	Name  string `bson:"name"`
+	Value string `bson:"value"`
+	Rank  string `bson:"rank"`
+}
+
 type Metadata struct {
 	Date      time.Time `bson:"date"`
 	Start_url string    `bson:"start_url"`
@@ -29,8 +35,8 @@ type Metadata struct {
 }
 
 type ScrapResult struct {
-	Iems     []Iem
-	Metadata Metadata
+	ScrappedData any
+	Metadata     Metadata
 }
 
 type config struct {
@@ -42,8 +48,14 @@ type config struct {
 	}
 }
 
-func initScrapper() *colly.Collector {
-	clt := colly.NewCollector()
+func initScrapper(url string) *colly.Collector {
+	url_split := strings.SplitAfter(url, "/")
+	base_url := url_split[1] + url_split[2]
+	base_url_trim := base_url[1 : len(base_url)-1]
+
+	clt := colly.NewCollector(
+		colly.AllowedDomains(base_url_trim),
+	)
 
 	clt.OnError(func(_ *colly.Response, err error) {
 		fmt.Fprintf(os.Stderr, "error while scraping the website: %v \n", err)
@@ -91,9 +103,23 @@ func main() {
 		os.Exit(1)
 	}
 	iems_collection := db_connection.Database("go-iemscrapper").Collection("iems")
+	rank_crinnacle_collection := db_connection.Database("go-iemscrapper").Collection("rank_crinnacle")
 
 	iem_list := []Iem{}
-	clt := initScrapper()
+	ranking_list := []Ranking{}
+	clt := initScrapper(cfg.website.url)
+	crin := initScrapper("https://crinacle.com/")
+
+	crin.OnHTML("#tablepress-4", func(e *colly.HTMLElement) {
+		name := e.ChildTexts(".column-3")
+		value := e.ChildTexts(".column-2")
+		rank := e.ChildTexts(".column-1")
+
+		for i := range name {
+			ranking_list = append(ranking_list, Ranking{name[i], value[i], rank[i]})
+		}
+	})
+
 	//scrap data from current page
 	clt.OnHTML(".productitem--info", func(e *colly.HTMLElement) {
 		title := e.ChildText(".productitem--title")
@@ -117,22 +143,30 @@ func main() {
 			price_discounted = price_original
 		}
 
-		// fmt.Println("Im appending!")
+		fmt.Println("Im appending!")
 		iem_list = append(iem_list, Iem{title, price_original, price_discounted, is_unreleased})
 	})
 
 	//visit next page
 	clt.OnHTML(".pagination--next", func(e *colly.HTMLElement) {
-		urlSplit := strings.SplitAfter(e.Request.URL.String(), "/")
-		baseUrl := urlSplit[0] + urlSplit[1] + urlSplit[2]
-		nextUrl := baseUrl + e.ChildAttr("a", "href")
-		e.Request.Visit(nextUrl)
+		url_split := strings.SplitAfter(e.Request.URL.String(), "/")
+		base_url := url_split[0] + url_split[1] + url_split[2]
+		next_url := base_url + e.ChildAttr("a", "href")
+		e.Request.Visit(next_url)
 	})
 	clt.Visit(cfg.website.url)
+	crin.Visit("https://crinacle.com/rankings/iems/")
 
-	result := ScrapResult{iem_list, Metadata{time.Now(), cfg.website.url, 1}}
+	result_iems := ScrapResult{iem_list, Metadata{time.Now(), cfg.website.url, 1}}
+	result_rank_crinnacle := ScrapResult{ranking_list, Metadata{time.Now(), cfg.website.url, 1}}
 
-	response, err := iems_collection.InsertOne(context.TODO(), result)
+	response, err := iems_collection.InsertOne(context.TODO(), result_iems)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(response.InsertedID)
+
+	response, err = rank_crinnacle_collection.InsertOne(context.TODO(), result_rank_crinnacle)
 	if err != nil {
 		panic(err)
 	}
